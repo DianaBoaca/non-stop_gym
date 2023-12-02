024fa0314fed3f47c62633630360b4a2899cec62
DianaBoaca <diana.boaca@student.upt.ro>
2023-12-02 18:29:43 +0200

    redirectionare ecran in functie de rol - admin sau client

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:non_stop_gym/screens/AuthScreen.dart';
-import 'package:non_stop_gym/screens/ClientHomeScreen.dart';
 import 'package:non_stop_gym/screens/SplashScreen.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -10,6 +10,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
   );
   runApp(const App());
 }
@@ -31,10 +34,6 @@ class App extends StatelessWidget {
         builder: (ctx, snapshot) {
           if (snapshot.connectionState  == ConnectionState.waiting) {
             return const SplashScreen();
-          }
-
-          if (snapshot.hasData) {
-            return const ClientHomeScreen();
           }
 
           return const AuthScreen();
